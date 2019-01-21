1687fcd2f3f2ab179e7f9c5763522e4bc948bce4
slamtrade <42183416+slamtrade@users.noreply.github.com>
2019-01-21 22:11:35 +0530

    Delete EmailRepository.java

--- EmailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mongodb.mongodbdemo.repository;
-
-import com.mongodb.mongodbdemo.document.Feedbackform;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EmailRepository extends MongoRepository<Feedbackform, Integer> {
-}
